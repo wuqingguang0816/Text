513fdc493fd15e01b932f1b7286b89220e0c62e8
wuqingguang0816 <956499199qq.com>
2018-12-13 23:39:05 +0800

    filst

---
+++ src/template/mods/star.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="star" :class="starType">
+    <span v-for="(itemClass,index) in itemClasses()" :class="itemClass" class="star-item" :key="index"></span>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['score', 'size'],
+    methods: {
+      itemClasses () {
+        let result = []
+        let score = Math.floor(this.score)
+        let hasDecimal = (this.score - score) >= 0.5
+        for (let i = 0; i < score; i++) {
+          result.push('on')
+        }
+        if (hasDecimal) {
+          result.push('half')
+        }
+        while (result.length < 5) {
+          result.push('off')
+        }
+        return result
+      }
+    },
+    computed: {
+      starType () {
+        return `star-${this.size}`
+      }
+    }
+  }
+</script>
